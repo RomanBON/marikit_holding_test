4260fa842ca499fb40a9d02ef6bf83508b8a0bbd
Roman Bondarenko <bondarenko.r@citilink.ru>
2020-02-01 18:32:52 +0300

    Updates

---
+++ precache-manifest.b7754a953fd447f73c6f35697b600c06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021d234880cd6bbf1b4a1ba5f3a95a1a",
+    "url": "/marikit_holding_test/index.html"
+  },
+  {
+    "revision": "009479cfcfbe16b7c2da",
+    "url": "/marikit_holding_test/static/css/main.22b55418.chunk.css"
+  },
+  {
+    "revision": "bb879b03f25b14c373df",
+    "url": "/marikit_holding_test/static/js/2.b8389005.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/marikit_holding_test/static/js/2.b8389005.chunk.js.LICENSE"
+  },
+  {
+    "revision": "009479cfcfbe16b7c2da",
+    "url": "/marikit_holding_test/static/js/main.a729859d.chunk.js"
+  },
+  {
+    "revision": "ce9b3b2bfcffcf8131fb",
+    "url": "/marikit_holding_test/static/js/runtime-main.00836308.js"
+  },
+  {
+    "revision": "00e1caa6c2e002d078eff5277acb939b",
+    "url": "/marikit_holding_test/static/media/spinner.00e1caa6.svg"
+  }
+]);
